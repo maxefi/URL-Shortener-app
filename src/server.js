c0d8744d9a530d28445c573499b8a17bafbbd7ea
maxefi <maxefi@mail.ru>
2019-03-01 13:30:52 +0300

    set up basic express server, add route for html and static css

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(htmlPath);
+});
+
+app.set('port', process.env.PORT || 4100);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
