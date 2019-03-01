336b8b9b0ee9e53aa9ce14b707c342ca25f7ad03
maxefi <maxefi@mail.ru>
2019-03-01 14:54:34 +0300

    add BE url validation

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const dns = require('dns');
 
 const app = express();
 
@@ -18,5 +19,18 @@ const server = app.listen(app.get('port'), () => {
 });
 
 app.post('/new', (req, res) => {
-  console.log({ BODY: req.body });
+  let originalUrl;
+
+  try {
+    // node v11+
+    originalUrl = new URL(req.body.url);
+  } catch (err) {
+    return res.status(400).send({ error: 'invalid URL' });
+  }
+
+  dns.lookup(originalUrl.hostname, err => {
+    if (err) {
+      return res.status(404).send({ error: 'Address not found' });
+    }
+  });
 });
